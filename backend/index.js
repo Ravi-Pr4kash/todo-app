fa626658e0b73ec80122aad41c973975d87c4dca
Ravi-Pr4kash <raviprakash0818@gmail.com>
2024-12-07 20:41:42 +0530

    server-listening

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 require("./connection/conn.js")
+const auth = require("./routes/auth.js")
  
 
 app.get('/', (req,res)=>{
     res.send("Hello");
 })
 
-app.listen(1000, ()=>{
+app.use(express.json())
+app.use("/api/v1", auth )
+app.listen(3000, ()=>{
     console.log("Server Started ")
 })
